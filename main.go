6ac4069069d7b9c53d61764c0ac80de8c9c95644
Per Buer <per.buer@gmail.com>
2022-02-18 16:45:35 +0100

    ...

--- main.go
+++ main.go
@@ -3,19 +3,27 @@ package main
 import (
 	"errors"
 	"fmt"
+	"runtime"
 )
 
 type Merror struct {
-	msg   string
-	inner error
+	msg    string
+	inner  error
+	caller string
 }
 
 func (m Merror) Error() string {
-	return fmt.Sprintf("%s: %v", m.msg, m.inner)
+	return fmt.Sprintf("%s: %s %v", m.caller, m.msg, m.inner)
 }
 
 func New(msg string, inner error) Merror {
-	return Merror{msg: msg, inner: inner}
+	pc, _, _, ok := runtime.Caller(1)
+	details := runtime.FuncForPC(pc)
+	var caller string
+	if ok && details != nil {
+		caller = details.Name()
+	}
+	return Merror{msg: msg, inner: inner, caller: caller}
 }
 
 func main() {
@@ -30,7 +38,7 @@ func processFile() error {
 
 	err := openFile()
 	if err != nil {
-		return New("processFile opening file", err)
+		return New("opening file", err)
 	}
 	return nil
 }
